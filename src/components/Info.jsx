cba73bba0aec8cca885a86dcec68986c859da9bf
PRT28 <56737649+PRT28@users.noreply.github.com>
2021-12-18 20:43:20 +0530

    Update Info.jsx

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -9,36 +9,44 @@ function Info(props){
 
     let [data,setData]=useState([]);
     let { id } = useParams();
-
-    async function getData(){
-        const res = await axios.get("https://www.breakingbadapi.com/api/characters/"+id);
-        setData(res.data);
-        console.log(data);
-    }
+    
 
 
     useEffect(()=> { 
+        const getData= async() =>{
+            try{
+                const res = await axios.get("https://www.breakingbadapi.com/api/characters/"+id);
+                setData(res.data[0]);
+                console.log(data);
+            } catch(e){
+                console.log(e)
+            }
+        }
         getData();
-    },);
+    },[]);
+
+        if(data.appearance !== undefined){
 
         return(
-            <>
-             {data && (<div className="info">
-            <img src={data.img} alt="" />
-            <h1>{data.name}</h1>
-            <p>Appearance: {data.appearance}</p>
-            <p>Birthday: {data.birthday}</p>
-            <p>Category: {data.category}</p>
-            <p>Nickname: {data.nickname}</p>
-            <p>Occupation: {data.occupation}</p>
-            <p>Portrayed: {data.portrayed}</p>
-            <p>Status: {data.status}</p>
-        </div>)}</>
-        );
-    
-    
-
-   
+            <div className="info">
+                <img src={data.img} alt="" />
+                <h1>{data.name}</h1>
+                {data.appearance.length>0?<p>Appearance: {data.appearance.join(',')}</p>:<p>Appearance: None</p>}
+                {data.better_call_saul_appearance.length>0? <p>Better call saul appearance: {data.better_call_saul_appearance.join(',')}</p> : <p>Better call saul appearance: None</p>}
+                <p>Birthday: {data.birthday}</p>
+                <p>Category: {data.category}</p>
+                <p>Nickname: {data.nickname}</p>
+                {data.occupation.length>0?<p>Occupation: {data.occupation.join(',')}</p>:<p>Occupation: None</p>}
+                <p>Portrayed: {data.portrayed}</p>
+                <p>Status: {data.status}</p>
+            </div>
+    );
+        }
+        else{
+            return(
+                <div className="info"></div>
+            )
+        }
 }
 
 export default Info;
