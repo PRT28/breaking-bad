8e00d7a16b9936fe1b53b05356b738fe64e31846
PRT28 <56737649+PRT28@users.noreply.github.com>
2021-12-22 10:57:06 +0530

    next commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,33 @@
 import React from 'react';
 import {Button} from '@material-ui/core';
 import {Link} from 'react-router-dom';
+import cross from './cross.png';
 
 function Card(props){
-    return(
-        <div className="card">
-            <img src={props.image} alt=""></img>
-            <h1>{props.name}</h1>
-            <p>{props.status}</p>
-            <Link to={"/info/"+props.id}><Button variant="contained">View More..</Button> </Link>
-        </div>
-    )
+
+    if(props.status==="Alive"){
+        return(
+            <div className="card">
+                <img src={props.image} alt=""/>
+                <h1>{props.name}</h1>
+                <p>{props.status}</p>
+                <Link to={"/info/"+props.id}><Button variant="contained">View More</Button> </Link>
+            </div>
+        )
+    }else{
+        return(
+            <div className="card">
+                <div class="i">
+                    <img src={props.image} alt=""/>
+                    <img class="cross" src={cross} alt="" />
+                </div>
+                <h1>{props.name}</h1>
+                <p>{props.status}</p>
+                <Link to={"/info/"+props.id}><Button variant="contained">View More</Button> </Link>
+            </div>
+        )
+    }
+    
 }
 
 export default Card;
