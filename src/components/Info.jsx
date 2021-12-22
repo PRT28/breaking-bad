e3aca85dfbbefd02272b7cd42d040961722f1ee7
PRT28 <56737649+PRT28@users.noreply.github.com>
2021-12-22 12:48:20 +0530

    next commit

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -34,16 +34,22 @@ function Info(props){
 
         return(
             <div className="info">
-                <img src={data.img} alt="" />
-                <h1>{data.name}</h1>
-                {data.appearance.length>0?<p>Appearance: {data.appearance.join(',')}</p>:<p>Appearance: None</p>}
-                {data.better_call_saul_appearance.length>0? <p>Better call saul appearance: {data.better_call_saul_appearance.join(',')}</p> : <p>Better call saul appearance: None</p>}
-                <p>Birthday: {data.birthday}</p>
-                <p>Category: {data.category}</p>
-                <p>Nickname: {data.nickname}</p>
-                {data.occupation.length>0?<p>Occupation: {data.occupation.join(',')}</p>:<p>Occupation: None</p>}
-                <p>Portrayed: {data.portrayed}</p>
-                <p>Status: {data.status}</p>
+                <div className="info-container">
+                    <div className="column">
+                        <img src={data.img} alt="" />
+                    </div>
+                    <div className="column1">
+                        <h1>{data.name}</h1>
+                        {data.appearance.length>0?<p>Appearance: {data.appearance.join(',')}</p>:<p>Appearance: None</p>}
+                        {data.better_call_saul_appearance.length>0? <p>Better call saul appearance: {data.better_call_saul_appearance.join(',')}</p> : <p>Better call saul appearance: None</p>}
+                        <p>Birthday: {data.birthday}</p>
+                        <p>Category: {data.category}</p>
+                        <p>Nickname: {data.nickname}</p>
+                        {data.occupation.length>0?<p>Occupation: {data.occupation.join(',')}</p>:<p>Occupation: None</p>}
+                        <p>Portrayed: {data.portrayed}</p>
+                        <p>Status: {data.status}</p>
+                    </div>
+                </div>
             </div>
     );
         }
